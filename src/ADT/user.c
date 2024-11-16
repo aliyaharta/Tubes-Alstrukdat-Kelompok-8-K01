260906b347ffc2168cb2afde713ca09bd9121786
kepin <19623202@std.stei.itb.ac.id>
2024-11-16 18:01:55 +0700

    add ADT

---
+++ src/ADT/user.c
@@ -0,0 +1,30 @@
+#include "user.h"
+#include <stdio.h>
+
+void CreateUser(User *u, const Word *name, const Word *password, int money) {
+    for (int i = 0; i < name->Length && i < MAX_LEN - 1; i++) {
+        u->name[i] = name->TabWord[i];
+    }
+    u->name[name->Length] = '\0';
+
+    for (int i = 0; i < password->Length && i < MAX_LEN - 1; i++) {
+        u->password[i] = password->TabWord[i];
+    }
+    u->password[password->Length] = '\0';
+
+    u->money = money;
+
+}
+
+void DisplayUser(User u) {
+    printf("Name: %s, Money: %d\n", u.name, u.money);
+}
+
+booleanIsUsernameEqual(User *u, const Word *username) {
+    for (int i = 0; i < username->Length; i++) {
+        if (i >= MAX_LEN || username->TabWord[i] != u->name[i]) {
+            return false;
+        }
+    }
+    return (username->Length == 0 || u->name[username->Length] == '\0');
+}
