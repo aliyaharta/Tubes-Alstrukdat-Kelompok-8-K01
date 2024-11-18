5d833bd59fc2594ee3b4d827db3264a7b7f8841c
aliyaharta <167730631+aliyaharta@users.noreply.github.com>
2024-11-18 21:11:19 +0700

    Update display.c

--- src/display.c
+++ src/display.c
@@ -1,109 +1,44 @@
 #include <stdio.h>
 #include <time.h>
-#include "data.h"
 
-void delay(){
+void delay(int milisekon) {
     clock_t start_time = clock();
-    while (clock()<start_time+100000);
+    while (clock() < start_time + milisekon * CLOCKS_PER_SEC / 1000);
 }
+
+const char *ascii_art[] = {
+    " __    __    ___  _         __   ___   ___ ___    ___      ______   ___  ",
+    "|  T__T  T  /  _]| T       /  ] /   \\ |   T   T  /  _]    |      T /   \\ ",
+    "|  |  |  | /  [_ | |      /  / Y     Y| _   _ | /  [_     |      |Y     Y",
+    "|  |  |  |Y    _]| l___  /  /  |  O  ||  \\_/  |Y    _]    l_j  l_j|  O  |",
+    "l  `  '  !|   [_ |     T/   \\_ |     ||   |   ||   [_       |  |  |     |",
+    " \\      / |     T|     |\\     |l     !|   |   ||     T      |  |  l     !",
+    "  \\_/\\_/  l_____jl_____j \\____j \\___/ l___j___jl_____j      l__j   \\___/ ",
+    " ____  __ __  ____   ____   ___ ___   ____  ____  ______  __             ",             
+    "|    \\|  T  T|    \\ |    \\ |   T   T /    T|   \\ |      T|  T            ",     
+    "|  o  )  |  ||  D  )|  D  )| _   _ |Y  o  ||  D  )      ||  |            ",
+    "|   _/|  |  ||    / |    / |  \\_/  ||     ||    /l_j  l_j|__j            ", 
+    "|  |  |  :  ||    \\ |    \\ |   |   ||  _  ||    \\  |  |   __             ",   
+    "|  |  l     ||  .  Y|  .  Y|   |   ||  |  ||  .  Y |  |  |  T            ",  
+    "l__j   \\__,_jl__ \\_jl__ \\_jl___j___jl__j__jl__ \\ _jl__j  l__j            ",
+};
 
 void welcome_menu()
-{
-    delay();
-    printf(" __    __    ___  _         __   ___   ___ ___    ___      ______   ___  \n");
-    delay();
-    printf("|  T__T  T  /  _]| T       /  ] /   \ |   T   T  /  _]    |      T /   \ \n");
-    delay();
-    printf("|  |  |  | /  [_ | |      /  / Y     Y| _   _ | /  [_     |      |Y     Y\n");
-    delay();
-    printf("|  |  |  |Y    _]| l___  /  /  |  O  ||  \_/  |Y    _]    l_j  l_j|  O  |\n");
-    delay();
-    printf("l  `  '  !|   [_ |     T/   \_ |     ||   |   ||   [_       |  |  |     |\n");
-    delay();
-    printf(" \      / |     T|     |\     |l     !|   |   ||     T      |  |  l     !\n");
-    delay();
-    printf("  \_/\_/  l_____jl_____j \____j \___/ l___j___jl_____j      l__j   \___/ \n");
-    delay();
-    printf("                                                                         \n");     
-    delay();
-    printf("                                                                         \n");          
-    delay();                                                      
-    printf(" ____  __ __  ____   ____   ___ ___   ____  ____  ______  __             \n");    
-    delay();         
-    printf("|    \|  T  T|    \ |    \ |   T   T /    T|    \|      T|  T            \n");         
-    delay(); 
-    printf("|  o  )  |  ||  D  )|  D  )| _   _ |Y  o  ||  D  )      ||  |            \n"); 
-    delay();
-    printf("|   _/|  |  ||    / |    / |  \_/  ||     ||    /l_j  l_j|__j            \n");    
-    delay();   
-    printf("|  |  |  :  ||    \ |    \ |   |   ||  _  ||    \  |  |   __             \n");   
-    delay();
-    printf("|  |  l     ||  .  Y|  .  Y|   |   ||  |  ||  .  Y |  |  |  T            \n");    
-    delay();
-    printf("l__j   \__,_jl__j\_jl__j\_jl___j___jl__j__jl__j\_j l__j  l__j            \n");
-    delay();
-    printf("                                                                         \n");
-    delay();
-    printf("                                                                         \n");
-    delay();
-    printf("1. START : Untuk memulai sesi baru                                       \n");
-    delay();
-    printf("2. LOAD : Untuk memulai sesi berdasarkan file konfigurasi                \n");
-    delay();
-    printf("3. HELP : Untuk memberikan info selanjutnya                              \n");
-    delay();
-    printf("4. QUIT : Untuk keluar dari program                                      \n");
-    delay();
-    printf("\n");
-    delay();
+{   
+    for (int i = 0; i < sizeof(ascii_art) / sizeof(ascii_art[0]); i++) {
+        delay(100);
+        printf("%s\n", ascii_art[i]);
 }
-
-void keluar_game(){
-    delay();
-    printf(" ______    ___  ____   ____  ___ ___   ____                              \n");
-    delay();                             
-    printf("|      T  /  _]|    \ l    j|   T   T /    T                             \n"); 
-    delay();                            
-    printf("|      | /  [_ |  D  ) |  T | _   _ |Y  o  |                             \n");             
-    delay();                
-    printf("l_j  l_jY    _]|    /  |  | |  \_/  ||     |                             \n");         
-    delay();                    
-    printf("  |  |  |   [_ |    \  |  | |   |   ||  _  |                             \n");   
-    delay();                          
-    printf("  |  |  |     T|  .  Y j  l |   |   ||  |  |                             \n");                 
-    delay();            
-    printf("  l__j  l_____jl__j\_j|____jl___j___jl__j__j                             \n"); 
-    delay();                            
-    printf("                                                                         \n");        
-    delay();                
-    printf(" __  _   ____  _____ ____  __ __      ______    ___  _       ____  __ __ \n");
-    delay();
-    printf("|  l/ ] /    T/ ___/l    j|  T  T    |      T  /  _]| T     /    T|  T  T\n");
-    delay();
-    printf("|  ' / Y  o  (   \_  |  T |  l  |    |      | /  [_ | |    Y  o  ||  l  |\n");
-    delay();
-    printf("|    \ |     |\__  T |  | |  _  |    l_j  l_jY    _]| l___ |     ||  _  |\n");
-    delay();
-    printf("|     Y|  _  |/  \ | |  | |  |  |      |  |  |   [_ |     T|  _  ||  |  |\n");
-    delay();
-    printf("|  .  ||  |  |\    | j  l |  |  |      |  |  |     T|     ||  |  ||  |  |\n");
-    delay();
-    printf("l__j\_jl__j__j \___j|____jl__j__j      l__j  l_____jl_____jl__j__jl__j__j\n");
-    delay();
-    printf("                                                                         \n");
-    delay();
-    printf("____     ___  ____   ___ ___   ____  ____  ____                          \n");
-    delay();
-    printf("|    \   /  _]|    \ |   T   T /    Tl    j|    \                        \n");
-    delay();
-    printf("|  o  ) /  [_ |  D  )| _   _ |Y  o  | |  T |  _  Y                       \n");
-    delay();
-    printf("|     TY    _]|    / |  \_/  ||     | |  | |  |  |                       \n");
-    delay();
-    printf("|  O  ||   [_ |    \ |   |   ||  _  | |  | |  |  |                       \n");
-    delay();
-    printf("|     ||     T|  .  Y|   |   ||  |  | j  l |  |  |                       \n");
-    delay();
-    printf("l_____jl_____jl__j\_jl___j___jl__j__j|____jl__j__j                       \n");
-    delay();
+    printf("\n");
+    printf("\n");
+    printf("1. START : Untuk memulai sesi baru                                       \n");
+    delay(100);
+    printf("2. LOAD : Untuk memulai sesi berdasarkan file konfigurasi                \n");
+    delay(100);
+    printf("3. HELP : Untuk memberikan info selanjutnya                              \n");
+    delay(100);
+    printf("4. QUIT : Untuk keluar dari program                                      \n");
+    delay(100);
+    printf("\n");
+    delay(100);
 }
