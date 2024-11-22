bb1cc0c0d0d0ef6321c90fd36780537be926aac8
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-11-22 17:32:08 +0700

    Update challenge.h

--- src/function/challenge.h
+++ src/function/challenge.h
@@ -3,10 +3,18 @@
 
 #include <stdio.h>
 
+// Deklarasi variabel global
 extern const char *word_list[];
 extern const int word_count;
 extern int money;
 
+// Deklarasi fungsi
+/**
+ * I.S. Program menampilkan daftar challenge dan meminta input angka dari pengguna.
+ * F.S. Challenge yang dipilih akan dijalankan jika uang mencukupi, 
+ *      atau menampilkan pesan bahwa uang tidak cukup jika tidak mencukupi.
+ */
 void challenge();
+
 
 #endif // CHALLENGE_H
