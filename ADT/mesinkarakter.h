a4ff8500586ff011e8fb3cf3222713e02fa3c75c
aliyaharta <167730631+aliyaharta@users.noreply.github.com>
2024-11-22 17:04:50 +0700

    add keterangan

--- ADT/mesinkarakter.h
+++ ADT/mesinkarakter.h
@@ -9,11 +9,16 @@
 extern char currentChar;  // Karakter yang sedang diproses
 extern boolean EOP;       // End of Process, apakah sudah mencapai akhir input
 
-void START(char *filename);  // Inisialisasi mesin karakter (input bisa dari file atau stdin)
-void ADV();                  // Pindah ke karakter berikutnya
-char GetCC();                // Mengambil karakter saat ini
-boolean IsEOP();             // Mengecek apakah sudah sampai di akhir input
-boolean IsNewline(); // Mengecek apakah karakter saat ini adalah newline
+void START(char *filename);  
+// Fungsi ini digunakan untuk Inisialisasi Mesin Karakter (input bisa dari file atau stdin)
+void ADV();                  
+// Fungsi ini digunakan untuk pindah ke karakter berikutnya
+char GetCC();                
+// Fungsi ini digunakan untuk mengambil karakter saat ini
+boolean IsEOP();             
+// Fungsi ini digunakan untuk mengecek apakah sudah sampai di akhir input
+boolean IsNewline(); 
+// Fungsi ini digunakan untuk mengecek apakah karakter saat ini adalah newline
 
 
 #endif
