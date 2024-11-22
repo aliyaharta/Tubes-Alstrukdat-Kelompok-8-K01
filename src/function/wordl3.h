f69a3420d6a1ae2a21045a66cc27f6779fc423c6
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-11-22 17:26:04 +0700

    NAMBAHIN COMMENT

--- src/function/wordl3.h
+++ src/function/wordl3.h
@@ -12,6 +12,10 @@ extern const char *word_list[] = {"TRULY", "LUCKY", "BUILD", "PLUCK", "STORM"};
 extern int word_count = 5;           
 extern int money;               
 
+/* 
+I.S: Permainan W0RDL3 belum dimulai. Pemain memiliki sejumlah uang dan kesempatan bermain.
+F.S: Pemain berhasil menebak kata dalam jumlah kesempatan yang diberikan atau kehabisan kesempatan. Hasil permainan akan dicetak, dan jumlah uang pemain akan diperbarui jika menang.
+*/
 void wordl3();
 
 #endif 
