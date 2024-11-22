2a9fb8b47b20bdf095857fde6aa533bb338edc2f
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-11-22 17:33:05 +0700

    Update delay.h

--- src/function/delay.h
+++ src/function/delay.h
@@ -4,6 +4,14 @@
 
 #include <time.h>
 
+/**
+ * I.S.
+ * - `seconds` adalah bilangan integer positif yang merepresentasikan durasi waktu tunda dalam detik.
+ *
+ * F.S.
+ * - Program berhenti (tertunda) selama `seconds` detik sebelum melanjutkan eksekusi ke baris kode berikutnya.
+ * - Tidak ada perubahan pada variabel lain selama fungsi ini berjalan.
+ */
 void delay(int seconds);
 
 #endif // DELAY_H
