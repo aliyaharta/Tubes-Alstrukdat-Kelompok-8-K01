f82320951bcaf3cd7ea8ef0845e3df8b9203cee0
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-11-22 17:33:54 +0700

    Update quantumWordle.h

--- src/function/quantumWordle.h
+++ src/function/quantumWordle.h
@@ -14,6 +14,18 @@ extern const char *word_list[] = {"TRULY", "LUCKY", "BUILD", "PLUCK", "STORM"};
 extern int word_count = 5;      
 extern int money;               
 
+/**
+ * I.S.
+ * - Daftar kata tersedia di variabel global `word_list` dan jumlahnya di `word_count`.
+ * - Pemain memiliki uang di variabel global `money`.
+ * - Fungsi pendukung seperti `wordCheck`, `stringLength`, `toUpperCase`, `charCheck`, dan `stringCompare` telah terdefinisi.
+ * - Konstanta `WORD_LENGTH` mendefinisikan panjang setiap kata.
+ *
+ * F.S.
+ * - Pemain dapat menebak hingga `MAX_ATTEMPTS_QUANTUM` kali untuk menebak `WORD_COUNT` kata secara benar.
+ * - Jika pemain berhasil menebak semua kata dengan benar dalam jumlah kesempatan yang diberikan, mereka memenangkan permainan dan mendapatkan tambahan uang.
+ * - Jika pemain gagal menebak semua kata dengan benar, jawaban yang benar akan ditampilkan.
+ */
 void quantumWordle();
 
 #endif 
