0807ee082ea0fe3ae12cb301d4dfce1b13ebeb3d
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-12-17 21:50:26 +0700

    Update quantumWordle.c

--- src/function/quantumWordle.c
+++ src/function/quantumWordle.c
@@ -1,5 +1,14 @@
 #include "quantumWordle.h"
 #include "toUpperCase.h" 
+#include "inputUser.h"
+#include "compare.h"
+#include "mesinkata.h"
+#include "wordCheck.h"
+#include "charCheck.h"
+
+const char *word_list[] = {"TRULY", "LUCKY", "BUILD", "PLUCK", "STORM"}; 
+int word_count = 5;      
+int money = 0;  
 
 void quantumWordle() {
     #define WORD_COUNT 4
@@ -41,56 +50,70 @@ void quantumWordle() {
     while (kesempatan < MAX_ATTEMPTS_QUANTUM) {
         printf("Masukkan %d kata tebakan Anda (dipisah dengan spasi):\n", WORD_COUNT);
         for (int i = 0; i < WORD_COUNT; i++) {
-            scanf("%s", tebakan[i]);
-            int len = getLength(tebakan[i]);  
-            
-            if (len != WORD_LENGTH) {
-                printf("Kata harus berisi %d karakter!\n", WORD_LENGTH);
-                i--; 
-                continue; 
+            ArrayOfKata input = inputUser(); 
+
+            if (input.WordCount != WORD_COUNT) {
+            printf("Harap masukkan tepat %d kata!\n", WORD_COUNT);
+            continue;
             }
-            for (int j = 0; j < WORD_LENGTH; j++) {
-                tebakan[i][j] = toUpperCase(tebakan[i][j]); 
+
+            int validInput = 1;
+
+            for (int i = 0; i < WORD_COUNT; i++) {
+                if (getLength(input.kata[i]) != WORD_LENGTH) {
+                    printf("Setiap kata harus berisi %d karakter!\n", WORD_LENGTH);
+                    validInput = 0;
+                    break;
+                }
+
+                for (int j = 0; j < WORD_LENGTH; j++) {
+                    tebakan[i][j] = toUpperCase(input.kata[i][j]);
+                }
+                tebakan[i][WORD_LENGTH] = '\0'; 
             }
+
+            if (!validInput) {
+                continue;
+            }
+
+            int semua_benar = 1;
+
+            for (int i = 0; i < WORD_COUNT; i++) {
+                charCheck(jawaban[i], tebakan[i], hasil[kesempatan][i]);
+                if (stringCompare(tebakan[i], jawaban[i]) != 0) {
+                    semua_benar = 0;
+                }
+            }
+
+            printf("Hasil tebakan Anda:\n");
+            for (int i = 0; i <= kesempatan; i++) {
+                for (int j = 0; j < WORD_COUNT; j++) {
+                    printf("%s   ", hasil[i][j]);
+                }
+                printf("\n");
+            }
+            for (int i = kesempatan + 1; i < MAX_ATTEMPTS_QUANTUM; i++) {
+                for (int j = 0; j < WORD_COUNT; j++) {
+                    printf("_ _ _ _ _   ");
+                }
+                printf("\n");
+            }
+            printf("\n");
+
+            if (semua_benar) {
+                printf("Selamat, Anda menang! Semua kata berhasil ditebak.\n");
+                money += 2000;
+                printf("+2000 rupiah telah ditambahkan ke akun Anda.\n");
+                return;
+            }
+
+            kesempatan++;
         }
 
-        int semua_benar = 1;
-
+        printf("Boo! Anda kalah. Jawaban yang benar adalah:\n");
         for (int i = 0; i < WORD_COUNT; i++) {
-            charCheck(jawaban[i], tebakan[i], hasil[kesempatan][i]);
-            if (stringCompare(tebakan[i], jawaban[i]) != 0) {
-                semua_benar = 0;
-            }
-        }
-
-        printf("Hasil tebakan Anda:\n");
-        for (int i = 0; i <= kesempatan; i++) {
-            for (int j = 0; j < WORD_COUNT; j++) {
-                printf("%s   ", hasil[i][j]);
-            }
-            printf("\n");
-        }
-        for (int i = kesempatan + 1; i < MAX_ATTEMPTS_QUANTUM; i++) {
-            for (int j = 0; j < WORD_COUNT; j++) {
-                printf("_ _ _ _ _   ");
-            }
-            printf("\n");
+            printf("%s ", jawaban[i]);
         }
         printf("\n");
-
-        if (semua_benar) {
-            printf("Selamat, Anda menang! Semua kata berhasil ditebak.\n");
-            money += 2000;
-            printf("+2000 rupiah telah ditambahkan ke akun Anda.\n");
-            return;
-        }
-
-        kesempatan++;
     }
-
-    printf("Boo! Anda kalah. Jawaban yang benar adalah:\n");
-    for (int i = 0; i < WORD_COUNT; i++) {
-        printf("%s ", jawaban[i]);
-    }
-    printf("\n");
 }
