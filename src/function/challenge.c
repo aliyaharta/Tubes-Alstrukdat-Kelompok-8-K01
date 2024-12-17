e4bfd28303997e4b6ee57828d7dcd86814ba47d1
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-12-17 21:46:09 +0700

    Update challenge.c

--- src/function/challenge.c
+++ src/function/challenge.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include "challenge.h"
-
-const char *word_list[] = {"TRULY", "LUCKY", "BUILD", "PLUCK", "STORM"};
-const int word_count = 5;
-
-int money = 1000;
+#include "inputUser.h"
+#include "compare.h"
 
 void challenge (){
         int angka;
-        scanf("%d", &angka);
 
-        if (angka == 1){
-            if (money < 200){
-                printf("Uang yang dimiliki kurang! Silakan kerja terlebih dahulu.");
+        ArrayOfKata input = inputUser();
+
+        while(input.WordCount < 2 && input.WordCount > 0){
+            if (stringCompare(input.kata[0], "1")){
+                if (money < 200){
+                    printf("Uang yang dimiliki kurang! Silakan kerja terlebih dahulu.");
+                }
+                else{
+                    money -= 200;
+                    printf("Masukkan tebakanmu!\n");
+                    tebakangka();
+                }
             }
-            else{
-                money -= 200;
-                printf("Masukkan tebakanmu!\n");
-                tebakangka();
+            else if (stringCompare(input.kata[0], "2")){
+                if (money < 500){
+                    printf("Uang yang dimiliki kurang! Silakan cari challenge lain atau kerja terlebih dahulu.");
+                }
+                else{
+                    money-=500;
+                    wordl3();
+                }
             }
+            else if (stringCompare(input.kata[0], "3")){
+                if (money < 600){
+                    printf("Uang yang dimiliki kurang! Silakan cari challenge lain atau kerja terlebih dahulu.");
+                }
+                else {
+                    money-=600;
+                    quantumWordle();
+                }
+            }
+            ArrayOfKata input = inputUser();
         }
-        else if (angka == 2){
-            if (money < 500){
-                printf("Uang yang dimiliki kurang! Silakan cari challenge lain atau kerja terlebih dahulu.");
-            }
-            else{
-                money-=500;
-                wordl3();
-            }
-        }
-        else if (angka == 3){
-            if (money < 600){
-                printf("Uang yang dimiliki kurang! Silakan cari challenge lain atau kerja terlebih dahulu.");
-            }
-            else {
-                money-=600;
-                quantumWordle();
-            }
-        }
-    }
+}
