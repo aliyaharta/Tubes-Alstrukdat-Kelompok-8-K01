6109a1b34f11be42c44d86f18a43e33433cd3209
Nadia Apsarini <18223065@std.stei.itb.ac.id>
2024-12-20 12:27:05 +0700

    Update storerequest.c

--- function/storerequest.c
+++ function/storerequest.c
@@ -25,27 +25,25 @@ void storerequest(ListBarang barangList, Queue *queue){
             printf("Barang sudah ada di Toko\n");
         } 
         else {
-            while(!isFull(*queue)){
-                if(inQueue(*queue, &barang)) {
+            if(inQueue(*queue, &barang)) {
                 printf("Barang sudah ada di antrian\n");
             } 
-                else {
-                    if (!isFull(*queue)) {
-                        enqueue(queue, &barang);
-                        CreateBarang(&barang, 0);
-                        printf("Barang berhasil dimasukkan ke antrian: %s", queue->items[queue->tail]);
-                        printf("%s",queue->items[queue->head]);
+            else {
+                if (!isFull(*queue)) {
+                    enqueue(queue, &barang);
+                    CreateBarang(&barang, 0);
+                    printf("Barang berhasil dimasukkan ke antrian: %s", queue->items[queue->tail]);
+                    printf("%s",queue->items[queue->head]);
                     }
-                    else{
-                        char temp;
-                        dequeue(queue,&temp);
-                        enqueue(queue,&barang);
-                        CreateBarang(&barang, 0);
-                        printf("%s",queue->items[queue->tail]);
-                        printf("Barang berhasil dimasukkan ke antrian: %s", queue->items[queue->tail]);
-                        printf("%s",queue->items[queue->head]);
+                else{
+                    char temp;
+                    dequeue(queue,&temp);
+                    enqueue(queue,&barang);
+                    CreateBarang(&barang, 0);
+                    printf("%s",queue->items[queue->tail]);
+                    printf("Barang berhasil dimasukkan ke antrian: %s", queue->items[queue->tail]);
+                    printf("%s",queue->items[queue->head]);
                     }
-                }
             }
             // if (!isEmpty(*queue)) {
             // if (inQueue(*queue, &barang)) {
